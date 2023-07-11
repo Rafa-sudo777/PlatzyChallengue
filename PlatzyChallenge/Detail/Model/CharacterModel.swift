d07424101f21ba890f34f6b62c2b0aeb7cc529f5
Rafael Aviles Puebla <ravilesp@hotelxcaret.com>
2023-07-11 01:46:53 -0600

    Adding characterModel

---
+++ PlatzyChallenge/Detail/Model/CharacterModel.swift
@@ -0,0 +1,10 @@
+//
+//  CharacterModel.swift
+//  PlatzyChallenge
+//
+//  Created by Rafael Aviles Puebla on 11/07/23.
+//
+
+struct CharacterModel: Decodable {
+    let name, gender, culture, born: String
+}
