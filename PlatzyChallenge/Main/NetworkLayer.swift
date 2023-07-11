a00566ae7e632339312827e6e33cf2894b701e0b
Rafael Aviles Puebla <ravilesp@hotelxcaret.com>
2023-07-11 01:41:47 -0600

    Adding Network Layer

---
+++ PlatzyChallenge/Main/NetworkLayer.swift
@@ -0,0 +1,36 @@
+//
+//  NetworkLayer.swift
+//  PlatzyChallenge
+//
+//  Created by Rafael Aviles Puebla on 11/07/23.
+//
+
+import Foundation
+
+final class NetworkLayer {
+    var books: [BookModel] = []
+    
+    func getBooks(url: String) async -> [BookModel] {
+        guard let url = URL(string: url) else { return [] }
+        let (data, _) = try! await URLSession.shared.data(from: url)
+        let booksModel = try! JSONDecoder().decode([BookModel].self, from: data)
+        return booksModel
+    }
+    
+    func getCharacters(url: String) async -> Character {
+        guard let url = URL(string: url) else {
+            return Character(name: String(),
+                             gender: String(),
+                             culture: String(),
+                             born: String())
+        }
+        let (data, _) = try! await URLSession.shared.data(from: url)
+        guard let authors = try? JSONDecoder().decode(Character.self, from: data) else {
+            return Character(name: String(),
+                             gender: String(),
+                             culture: String(),
+                             born: String())
+        }
+        return authors
+    }
+}
