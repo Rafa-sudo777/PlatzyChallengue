835538f728b8370513aed1a3c97c181ee0841a13
Rafael Aviles Puebla <ravilesp@hotelxcaret.com>
2023-07-11 01:33:06 -0600

    Creating MainView

--- PlatzyChallenge/ContentView.swift
+++ PlatzyChallenge/ContentView.swift
@@ -8,81 +8,63 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+struct MainView: View {
+    @ObservedObject private var viewModel = ListViewModel()
+    @State private var showToast = false
+    @State private var toastMessage = ""
+    @EnvironmentObject private var networkMonitor: NetworkMonitor
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+        VStack {
+            NavigationView {
+                if viewModel.books.isEmpty && networkMonitor.status == .disconnected {
+                    Text("No internet conection.\nPlease conect to network")
+                } else {
+                    List(Array(viewModel.books.enumerated()), id: \.element.self) { index, book in
+                        NavigationLink(destination: DetailView(book: book,
+                                                               index: index)) {
+                            VStack(alignment: .leading, spacing: 20, content: {
+                                Text(book.name)
+                                    .font(.title3)
+                                HStack() {
+                                    if let dateFormat = book.released.convertToDate() {
+                                        Text(dateFormat)
+                                            .font(.subheadline)
+                                            .bold()
+                                            .foregroundColor(.gray)
+                                        Spacer()
+                                        Text("Pag: " + String(book.numberOfPages))
+                                            .font(.caption2)
+                                            .foregroundColor(.gray)
+                                    }
+                                }
+                            })
+                        }
                     }
+                    .navigationTitle("Books")
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
         }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        .toast(message: toastMessage,
+               isShowing: $showToast,
+               duration: Toast.long)
+        .onChange(of: networkMonitor.status) { status in
+            switch status {
+                case .wifi:
+                    toastMessage = "Conected over Wifi"
+                case .cellular:
+                    toastMessage = "Conected over Cellular"
+            case .disconnected:
+                    toastMessage = "No internet connection"
             }
+               showToast = true
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        MainView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
