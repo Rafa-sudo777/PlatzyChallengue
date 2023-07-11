714a2a9349dc9f50f7802b8c46d99d7c4a96dca1
Rafael Aviles Puebla <ravilesp@hotelxcaret.com>
2023-07-11 14:24:47 -0600

    Persisting Data in coreData

--- PlatzyChallenge/Main/ViewModel/MainViewModel.swift
+++ PlatzyChallenge/Main/ViewModel/MainViewModel.swift
@@ -5,13 +5,54 @@
 //  Created by Rafael Aviles Puebla on 11/07/23.
 //
 
+import CoreData
 import Foundation
+
 @MainActor
 final class ListViewModel: ObservableObject {
     @Published var books: [BookModel] = []
     private let networkLayer = NetworkLayer()
     
     func fetchBooks() async throws {
+        let context = PersistenceController.shared.container.viewContext
+        let fetchRequest: NSFetchRequest<BookEntity> = BookEntity.fetchRequest()
+
+        do {
+            let books = try context.fetch(fetchRequest)
+            if !books.isEmpty {
+                self.books = books.map { BookModel(coreDataBook: $0) }
+                return
+            }
+        } catch {
+            print("Error fetching from Core Data: \(error)")
+        }
+
         self.books = await self.networkLayer.getBooks(url: "https://www.anapioficeandfire.com/api/books")
+
+        await context.perform {
+            self.books.forEach { bookModel in
+                _ = bookModel.toCoreDataBook(context: context)
+            }
+            do {
+                try context.save()
+            } catch let error {
+                print("Error saving to Core Data: \(error)")
+            }
+        }
+    }
+}
+
+extension BookModel {
+    func toCoreDataBook(context: NSManagedObjectContext) -> BookEntity {
+        let book = BookEntity(context: context)
+        book.name = name
+        book.numberOfPages = Int16(numberOfPages)
+        book.publisher = publisher
+        book.countrys = NSSet(array: authors.map { $0 })
+        book.mediaType = mediaType
+        book.released = released
+        book.authors = NSSet(array: authors.map { $0 })
+        book.characters = NSSet(array: characters.map { $0 })
+        return book
     }
 }
