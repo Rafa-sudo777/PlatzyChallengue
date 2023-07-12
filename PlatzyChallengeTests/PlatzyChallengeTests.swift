5e6262953af8a4450cf2e006b0fdc082cfc0bb68
Rafael Aviles Puebla <ravilesp@hotelxcaret.com>
2023-07-11 21:02:43 -0600

    creating first test successful

--- PlatzyChallengeTests/PlatzyChallengeTests.swift
+++ PlatzyChallengeTests/PlatzyChallengeTests.swift
@@ -6,12 +6,14 @@
 //
 
 import XCTest
+import CoreData
 @testable import PlatzyChallenge
 
-final class PlatzyChallengeTests: XCTestCase {
+@MainActor final class PlatzyChallengeTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        clearCoreData()
     }
 
     override func tearDownWithError() throws {
@@ -33,8 +35,63 @@ final class PlatzyChallengeTests: XCTestCase {
         }
     }
     
-    @MainActor private func makeSut(date: Date? = Date(),
-                                    file: StaticString = #file,
+    func test_fetchBooks_shouldSaveBooksInCoreData() async throws {
+        let sutComponents = makeSut()
+        sutComponents.retreiveBooksStub.books = listBooks.sorted { $0.name < $1.name }
+        let context = PersistenceController.shared.container.viewContext
+        do {
+            try await sutComponents.sut.fetchBooks()
+            
+            let fetchRequest: NSFetchRequest<BookEntity> = BookEntity.fetchRequest()
+            let books = try context.fetch(fetchRequest)
+            let fetchedBooks = books.sorted { $0.name! < $1.name! }
+            XCTAssertEqual(books.count, listBooks.count)
+            XCTAssertEqual(fetchedBooks.first?.name, sutComponents.retreiveBooksStub.books.first?.name)
+        } catch {
+            XCTFail("Failed fetching from Core Data: \(error)")
+        }
+    }
+    
+    private func clearCoreData() {
+        let context = PersistenceController.shared.container.viewContext
+        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = BookEntity.fetchRequest()
+        let batchDeleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+
+        do {
+            try context.execute(batchDeleteRequest)
+        } catch {
+            print("Error deleting from Core Data: \(error)")
+        }
+    }
+    
+    private var listBooks: [BookModel] {
+        [BookModel(name: "A Game of Thrones",
+                   authors: [.georgeRRMartin],
+                   numberOfPages: 694,
+                   publisher: "Bantam Books",
+                   country: .unitedStates,
+                   mediaType: "Hardcover",
+                   released: "1996-08-01",
+                   characters: ["Eddard Stark", "Daenerys Targaryen", "Jon Snow"]),
+         BookModel(name: "A Clash of Kings",
+                   authors: [.georgeRRMartin],
+                   numberOfPages: 768,
+                   publisher: "Bantam Books",
+                   country: .unitedStates,
+                   mediaType: "Hardcover",
+                   released: "1998-11-16",
+                   characters: ["Tyrion Lannister", "Arya Stark", "Stannis Baratheon"]),
+         BookModel(name: "A Storm of Swords",
+                   authors: [.georgeRRMartin],
+                   numberOfPages: 973,
+                   publisher: "Bantam Books",
+                   country: .unitedStates,
+                   mediaType: "Hardcover",
+                   released: "2000-08-08",
+                   characters: ["Bran Stark", "Jaime Lannister", "Catelyn Stark"])]
+    }
+    
+    private func makeSut(file: StaticString = #file,
                                     line: UInt = #line) -> (sut: ListViewModel,
                                                             retreiveBooksStub: RetreiveBooksStub) {
         
